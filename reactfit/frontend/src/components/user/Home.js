87c15ff947ee2241433a3b995e8b214628f2ddc0
MarianDanaila <mariandanaila01@gmail.com>
2023-04-08 14:47:56 +0200

    Refactor Home Page

--- reactfit/frontend/src/components/user/Home.js
+++ reactfit/frontend/src/components/user/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import UserService from "../../services/UserService";
-import logo from "./logo.jpg";
+import logo from "../../assets/logo.png";
 import { Link } from "react-router-dom";
 import AuthService from "../../services/AuthService";
 
@@ -38,43 +38,43 @@ export default class Home extends Component {
   render() {
     const currentUser = this.state.currentUser;
     return (
-      <div>
-        <div className="px-4 py-5 my-5 text-center">
-          <img
-            className="d-block mx-auto mb-4"
-            src={logo}
-            alt="Logo"
-            width="100"
-            height="100"
-            style={{ borderRadius: "50%" }}
-          />
-          <h1 className="display-5 fw-bold">Welcome to ReactFit</h1>
-          <div className="col-lg-6 mx-auto">
-            <p className="lead mb-4">
-              Quickly design and customize responsive mobile-first sites with
-              Bootstrap, the world's most popular front-end open source toolkit,
-              featuring Sass variables and mixins, responsive grid system,
-              extensive prebuilt components, and powerful JavaScript plugins.
-            </p>
-            {!currentUser && (
-              <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                <Link
-                  to={"/login"}
-                  className="btn btn-primary btn-lg px-4 gap-3"
-                  role="button"
-                >
-                  Login
-                </Link>
-                <Link
-                  to={"/register"}
-                  className="btn btn-outline-secondary btn-lg px-4"
-                  role="button"
-                >
-                  Sign Up
-                </Link>
-              </div>
-            )}
-          </div>
+      <div className="px-4 py-5 my-5 text-center">
+        <img
+          className="d-block mx-auto mb-4"
+          src={logo}
+          alt="Logo"
+          width="150"
+          height="150"
+          style={{ borderRadius: "50%" }}
+        />
+        <h1 className="fw-bold">Welcome to ReactFit</h1>
+        <div className="col-lg-6 mx-auto">
+          <p className="lead mb-4">
+            We're committed to helping you achieve your health and wellness
+            goals by providing you with the tools, resources, and support you
+            need to make positive changes in your life. Whether you're looking
+            to improve your diet, increase your physical activity, manage
+            stress, or just live a healthier, happier life, we've got you
+            covered.
+          </p>
+          {!currentUser && (
+            <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+              <Link
+                to={"/login"}
+                className="btn btn-primary btn-lg px-4"
+                role="button"
+              >
+                Login
+              </Link>
+              <Link
+                to={"/register"}
+                className="btn btn-outline-secondary btn-lg px-4"
+                role="button"
+              >
+                Sign Up
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     );
